9b5a7589a4dbaac1a9535e6441aab8524c9d6d4a
kazhamiakin <kazhamiakin@gmail.com>
2025-05-21 23:27:20 +0200

    #fix: get rid of CDN resources (fonts and icons)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import * as mdb from 'mdb-ui-kit'; // lib
 window.mdb = mdb;
 
 import './assets/scss/global.scss';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/500.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
